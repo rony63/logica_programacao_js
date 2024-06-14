36fa8f44c25de364095505a8dc9b0945f0aafd55
rony63 <ronymcall@gmail.com>
2024-06-14 13:53:49 -0300

    Adicionando os arquivos a supervisão do git

---
+++ app.js
@@ -0,0 +1,24 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 9;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+// Enquanto o chute não for igual ao n.s.
+while(chute != numeroSecreto){
+    chute = prompt('Escolha um número entre 1 a 10');
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        if(tentativas > 1){
+            alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
+        } else {
+            alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa!`);
+        }
+    } else {
+        if(numeroSecreto > chute) {
+            alert(`O número secreto é maior que ${chute}`);
+        }else {
+            alert(`O número secreto é menor que ${chute}`);
+        }
+        tentativas++;
+    }
+}
